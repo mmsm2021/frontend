7ea486b4c8c4bb39085c58dd85a8607d8b3def7b
Ellingduck <markus.elling@live.dk>
2021-05-05 14:20:10 +0200

    A bit more boilerplate stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import "./Container.css";
+class Container extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: undefined
+    }
+  }
+  render() {
+    return(
+        <div className="c-main">
+          <h1>{this.props.company}</h1>
+        </div>
+    )
+  }
+}
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container company="Café Crystal Methodic" />
       </header>
+
     </div>
+
   );
 }
 
