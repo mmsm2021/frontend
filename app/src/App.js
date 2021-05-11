2ed679b69bee8939254d5e07201785d1735e7cfe
Markus Johan Elling <markus.elling@aak.com>
2021-05-11 14:49:16 +0200

    Layout
    Changed the layout a bit

--- app/src/App.js
+++ app/src/App.js
@@ -1,92 +1,16 @@
 import './App.css';
 import React from "react";
-import {useAuth0} from "@auth0/auth0-react";
-import {Button} from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import {Avatar} from "@material-ui/core";
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-    large: {
-        width: theme.spacing(7),
-        height: theme.spacing(7),
-    },
-}));
-
-const Profile = () => {
-    const { user, isAuthenticated, isLoading, getIdTokenClaims } = useAuth0();
-    const classes = useStyles();
-/*    if (isLoading) {
-        return <div>Loading ...</div>;
-    }*/
-    if (!isAuthenticated){
-        return <LoginButton/>
-    }
-    const accessToken = getIdTokenClaims("__raw");
-    console.log(accessToken);
-
-    return (
-
-        isAuthenticated && (
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" className={classes.title}>
-
-                        </Typography>
-                        <Avatar src={user.picture} alt={user.name} className={classes.large}/>
-                        <LogoutButton/>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        )
-    );
-};
-const LoginButton = () =>{
-    const { loginWithRedirect } = useAuth0();
-
-        return <Button variant="contained"
-                       color="primary"
-                       size="large"
-                       onClick={() => loginWithRedirect()}>Login</Button>;
-
-}
-const LogoutButton = () => {
-    const { logout } = useAuth0();
-
-    return (
-        <Button
-            variant="contained"
-            color="secondary"
-            size="large"
-            onClick={() => logout({ returnTo: window.location.origin })}>
-            Log ud
-        </Button>
-    );
-};
+import Navigation from "./Navigation";
 
 function App() {
 
     return (
-    <div className="Landing-page">
-        <Profile />
+        <div>
+        <Navigation />
+            <div className="Landing-page">
 
-    </div>
+            </div>
+        </div>
 
   );
 }
---
+++ app/src/Navigation.js
@@ -0,0 +1,74 @@
+import {useAuth0} from "@auth0/auth0-react";
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Navbar, Nav, NavDropdown, Form, FormControl} from "react-bootstrap";
+import Logo from "./fd_logo.svg";
+
+const Profile = () => {
+    const { user, isAuthenticated, isLoading, getIdTokenClaims } = useAuth0();
+
+    if (!isAuthenticated){
+        return <LoginButton/>
+    }
+    const accessToken = getIdTokenClaims();
+    console.log(accessToken);
+
+    return (
+        isAuthenticated && (
+            <Nav>
+            <NavDropdown title={user.name} id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Min profil</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Historik</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Indstillinger</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4" >
+                    <LogoutButton/>
+                </NavDropdown.Item>
+            </NavDropdown>
+            </Nav>
+        )
+    );
+};
+const LoginButton = () =>{
+    const { loginWithRedirect } = useAuth0();
+
+    return <Nav.Item>
+            <Button onClick={() => loginWithRedirect()}>Login</Button>
+            </Nav.Item>
+}
+const LogoutButton = () => {
+    const { logout } = useAuth0();
+
+    return (
+        <Nav.Item>
+            <Button onClick={() => logout({ returnTo: window.location.origin })}>
+            Log ud
+            </Button>
+        </Nav.Item>
+    );
+};
+
+class Navigation extends React.Component{
+    render() {
+        return(
+            <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg" sticky="top">
+                <Navbar.Brand href="#home">
+                    <img src={Logo}
+                            width="126"
+                            height="48"
+                            className="d-inline-block align-top"
+                            alt="Company Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                    <Profile />
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+export default Navigation;
